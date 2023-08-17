18f225371b44603febd98cf9f446a7b9488265e9
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2023-08-17 10:45:47 +0300

    Test 1

--- src/main/java/com/epam/test/autocode/Main.java
+++ src/main/java/com/epam/test/autocode/Main.java
@@ -1,6 +1,14 @@
 package com.epam.test.autocode;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        var list = List.of(1,2,3,4,5,6,7,8);
+        var integers = list.stream()
+                .filter(number -> number > 2)
+                .collect(Collectors.toSet());
+        integers.forEach(System.out::println);
     }
 }
